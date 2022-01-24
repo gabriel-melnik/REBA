f58a485e6a41d7ce178809abcbfc4e2e5a1391c7
andy <arteysoft@gmail.com>
2022-01-24 14:34:36 -0300

    primer commit

---
+++ clase1/src/main/java/edu/it/CursoSpringApplication.java
@@ -0,0 +1,13 @@
+package edu.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy
+public class CursoSpringApplication {
+	public static void main(String[] args) {
+		System.out.println("Bienvenidos al curso de REBA");
+	}
+}
